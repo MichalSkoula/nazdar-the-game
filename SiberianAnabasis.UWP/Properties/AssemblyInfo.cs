e348d269e79eb50d1c6ededee1c82d9003cdc437
MichalSkoula <michal@skoula.cz>
2022-07-28 01:42:47 +0200

    monogame update

--- SiberianAnabasis.UWP/Properties/AssemblyInfo.cs
+++ SiberianAnabasis.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyGame.UWP")]
+[assembly: AssemblyTitle("SiberianAnabasis.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MyGame.UWP")]
-[assembly: AssemblyProduct("MyGame.UWP")]
+[assembly: AssemblyCompany("SiberianAnabasis.UWP")]
+[assembly: AssemblyProduct("SiberianAnabasis.UWP")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
