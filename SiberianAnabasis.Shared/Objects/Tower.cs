0f68d33afb4759672ea822f01d0165c5099da35e
MichalSkoula <michal@skoula.cz>
2022-08-23 00:54:32 +0200

    towers can shoot

--- SiberianAnabasis.Shared/Objects/Tower.cs
+++ SiberianAnabasis.Shared/Objects/Tower.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using static SiberianAnabasis.Enums;
 using Microsoft.Xna.Framework;
+using SiberianAnabasis.Shared;
 
 namespace SiberianAnabasis.Objects
 {
@@ -11,24 +12,71 @@ namespace SiberianAnabasis.Objects
     {
         public const int Cost = 3;
         public const string Name = "Tower";
+        public bool CanShoot { get; set; } = false;
+        private int shootPower = 1;
+        private int shootRate = 60; // 0 fastest, 100 slowest
+
+        public List<Bullet> Bullets
+        {
+            get;
+            private set;
+        }
 
         public Tower(int x, int y, Building.Status status)
         {
             this.Sprite = Assets.Images["Tower"];
             this.Hitbox = new Rectangle(x, y, this.Sprite.Width, this.Sprite.Height);
             this.Status = status;
+            this.Bullets = new List<Bullet>();
             this.TimeToBuilt = 5;
+            this.Caliber = 50;
             this.Type = Building.Type.Tower;
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(this.Sprite, this.Hitbox, this.FinalColor);
+
+            // bullets
+            foreach (var bullet in this.Bullets)
+            {
+                bullet.Draw(spriteBatch);
+            }
+        }
+
+        public void PrepareToShoot(Direction direction, int enemyX, int range)
+        {
+            this.Direction = direction;
+
+            float howFar = Math.Abs(enemyX - this.X) / (float)range;
+            this.shootPower = (int)(howFar * 10) / 2; // convert 0-1 float to 1-4 int
+            if (this.shootPower == 0)
+            {
+                this.shootPower = 1;
+            }
         }
 
         public new void Update(float deltaTime)
         {
             base.Update(deltaTime);
+
+            if (this.CanShoot && Game1.GlobalTimer % this.shootRate == 0)
+            {
+                this.Bullets.Add(new Bullet(
+                    this.X + this.Width / 2,
+                    this.Y + this.Height / 4,
+                    this.Direction,
+                    this.Caliber,
+                    BulletType.Cannonball,
+                    this.shootPower
+                ));
+            }
+
+            foreach (var bullet in this.Bullets)
+            {
+                bullet.Update(deltaTime);
+            }
+            this.Bullets.RemoveAll(p => p.ToDelete);
         }
     }
 }
