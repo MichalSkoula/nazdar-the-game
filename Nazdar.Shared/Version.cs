d86714b75224ec88aa548cfeca3acd06a06e68dc
MichalSkoula <michal@skoula.cz>
2022-12-03 23:14:34 +0100

    version number

---
+++ Nazdar.Shared/Version.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nazdar.Shared
+{
+    public class Version
+    {
+        public const string Number = "0.8";
+    }
+}
