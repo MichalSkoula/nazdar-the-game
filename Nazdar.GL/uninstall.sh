48aa4b4a39047235ea96e9f3a382feee23d9a69d
MichalSkoula <michal@skoula.cz>
2024-01-01 22:01:54 +0100

    Icon file for linux enthusiasts

--- Nazdar.GL/uninstall.sh
+++ Nazdar.GL/uninstall.sh
@@ -3,11 +3,6 @@
 INSTALLATION_DIR=$(echo $HOME)/.nazdar
 CURRENT_DIR=$(pwd)
 
-if [ "$INSTALLATION_DIR" != "$CURRENT_DIR" ]; then
-	echo "Run this from installation directory"
-	exit 1
-fi
-
 echo "Removing Nazdar..."
 rm -r $INSTALLATION_DIR
 rm -r $(echo $HOME)/.local/share/applications/nazdar.desktop
