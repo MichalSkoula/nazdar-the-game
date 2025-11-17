2ddfd60487fbf467df546f56cbc6aebf839646ab
MichalSkoula <michal@skoula.cz>
2025-11-17 21:10:21 +0100

    Revert "DX version with touch support 2"
    This reverts commit d362bc4ccc0a650af62150be35d32e4bc5e11228.

--- Nazdar.Shared/Translation/TranslationEN.cs
+++ Nazdar.Shared/Translation/TranslationEN.cs
@@ -16,6 +16,7 @@ namespace Nazdar.Shared.Translation
                 { "menu.music", "Music:" },
                 { "menu.sounds", "Sounds:" },
                 { "menu.vibrations", "Vibrations:" },
+                { "menu.fullscreen", "Fullscreen:" },
                 { "menu.language", "Language:" },
                 { "menu.exit", "Exit" },
                 { "menu.on", "On" },
